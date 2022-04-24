954d34b8cd44fdb204511146ee43c1191dee5aec
Vinicius-SO <vhisoares39@gmail.com>
2022-04-24 20:37:03 -0300

    Header component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,12 +1,11 @@
 import styles from './styles.module.scss'
-
 export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a>Home</a>
+                    <a className={styles.active}>Home</a>
                     <a>Posts</a>
                 </nav>
             </div>
