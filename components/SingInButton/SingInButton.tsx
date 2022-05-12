5cc97924929c3ba346d80a10c999636d26a01f28
Vinicius-SO <vhisoares39@gmail.com>
2022-05-12 20:17:39 -0300

    SingIn and Out

--- components/SingInButton/SingInButton.tsx
+++ components/SingInButton/SingInButton.tsx
@@ -1,15 +1,19 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn, useSession } from "next-auth/react"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 import styles from './styles.module.scss'
 
 export function SingInButton (){
     const { data: session, status } = useSession()
-    return session ? (
-        <button className={styles.singInButton}>
+    console.log(status)
+    return status === 'authenticated' ? (
+        <button
+         className={styles.singInButton}
+         onClick={()=>signOut()}
+         >
             <FaGithub color='#04d361'/>
-            Vinicius Soares
+                {session.user.name}
             <FiX color='#737380' className={styles.closeIcon}/>
         </button>
     ):(
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { SessionProvider as NextAuthProvider} from 'next-auth/react'
 import { AppProps} from 'next/app'
 import { Header } from '../components/header/Header'
 import '../styles/global.scss'
@@ -5,8 +6,10 @@ import '../styles/global.scss'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <NextAuthProvider session={pageProps.session}>
+        <Header />
+        <Component {...pageProps} />
+      </NextAuthProvider>
     </>
   )
 }
