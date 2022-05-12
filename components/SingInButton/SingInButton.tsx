b799bf0e9d34a291d8fed18bfe7effb10c94acfe
Vinicius-SO <vhisoares39@gmail.com>
2022-05-12 18:02:27 -0300

    NextAuth

--- components/SingInButton/SingInButton.tsx
+++ components/SingInButton/SingInButton.tsx
@@ -1,18 +1,23 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession } from "next-auth/react"
 
 import styles from './styles.module.scss'
 
 export function SingInButton (){
-    const isUserLoggedIn = true; 
-    return isUserLoggedIn ? (
+    const { data: session, status } = useSession()
+    return session ? (
         <button className={styles.singInButton}>
             <FaGithub color='#04d361'/>
             Vinicius Soares
             <FiX color='#737380' className={styles.closeIcon}/>
         </button>
     ):(
-        <button className={styles.singInButton}>
+        <button 
+        type='button'
+        className={styles.singInButton}
+        onClick={()=>signIn('github')}
+        >
         <FaGithub color='#eba417'/>
         Sing in With Github
     </button>
