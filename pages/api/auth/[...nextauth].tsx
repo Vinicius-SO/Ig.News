c00862335642bd7eeb48a57d0379876acee9bbb0
Vinicius-SO <vhisoares39@gmail.com>
2022-05-19 11:53:31 -0300

    Faunadb Create Operation

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,10 @@
+import { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna"  
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -13,5 +18,21 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, }) {
+      const { email } = user
+      try{
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: {email}}
+          )
+        )
+        return true
+      } catch{
+        return false
+      }
+    },
+  }
 })
 
