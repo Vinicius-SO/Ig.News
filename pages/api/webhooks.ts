0dff7aae48379581776cf642a120a7c0bfbc4b35
Vinicius-SO <vhisoares39@gmail.com>
2022-06-01 23:20:36 -0300

    Readable Stripe

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -1,6 +1,42 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from 'stream'
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
 
-export default (req:NextApiRequest, res:NextApiResponse) => {
-    console.log('evento recebido')
-    res.status(200).json({ ok: true })
+async function buffer(readable: Readable) {
+    const chuncks = [];
+
+    for await (const chunck of readable){
+        chuncks.push(
+            typeof chunck === 'string' ? Buffer.from(chunck) : chunck
+        );
+    }
+
+    return Buffer.concat(chuncks);
+}
+
+export const config = {
+    api:{
+        bodyParser: false
+    }
+}
+
+export default async (req:NextApiRequest, res:NextApiResponse) => {
+    if (req.method === 'POST'){
+        const buf = await buffer(req)
+        const secret = req.headers['stripe-signature']
+
+        let event: Stripe.Event;
+
+        try{
+            event = stripe.webhooks.constructEvent(buf,secret, process.env.STRIPE_WEBHOOK_SECRET)
+        }catch (err){
+            return res.status(400).send(`webhook error: ${err.message}`);
+        }
+        
+        res.status(200).json({ ok: true })
+    }else{
+        res.setHeader('Allow', 'POST')
+        res.status(405).end('Method not allowed')
+    }
 }
