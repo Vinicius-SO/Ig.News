16732da856496a25d547b43c13dfffbf66491a4d
Vinicius-SO <vhisoares39@gmail.com>
2022-06-13 20:24:05 -0300

    Getting data from prismic

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,5 +1,8 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import styles from './styles.module.scss'
+import * as Prismic from '@prismicio/client'
+import { getPrismicClient } from '../../services/prismic'
 
 export default function Posts(){
     return (
@@ -33,4 +36,23 @@ export default function Posts(){
             </main>
         </>
     )
+}
+
+export const getStaticProps:GetStaticProps = async()=>{
+    
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+
+        Prismic.predicates.at('document.type', 'publication')
+    ],{
+        fetch: ['publication.title', 'publication.content'],
+        pageSize: 100,
+    }
+    )
+
+    console.log(response)
+    return{
+        props:{}
+    }
 }
