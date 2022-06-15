27ab56821b1d6004e8997731b1032aa318aab815
Vinicius-SO <vhisoares39@gmail.com>
2022-06-14 22:42:41 -0300

    active link

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,17 +1,23 @@
-import { SingInButton } from '../SingInButton/SingInButton';
 import Link from 'next/link'
+import { useRouter } from 'next/router';
+
+import { SingInButton } from '../SingInButton/SingInButton';
+
 import styles from './styles.module.scss'
+
 export function Header() {
+    const { asPath } = useRouter()
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
                     <Link  href='/'>
-                        <a className={styles.active}>Home</a>
+                        <a className={asPath === '/' ? styles.active : ''}>Home</a>
                     </Link>
-                    <Link href='/posts' prefetch>
-                        <a >Posts</a>
+                    <Link href='/posts'>
+                        <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>
                     </Link>
                 </nav>
                 <SingInButton></SingInButton>
